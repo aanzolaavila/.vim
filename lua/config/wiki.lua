fb6e17668fad842f6f7ad5ddb09ae8d4805728ad
Alejandro Anzola <me@alejandroanzola.info>
2023-01-02 01:12:50 -0500

    creates wiki directory if not existent

--- lua/config/wiki.lua
+++ lua/config/wiki.lua
@@ -1,8 +1,12 @@
 local M = {}
 
 function M.setup()
-  -- TODO: add command to create directory as wiki does not create it
-  vim.g.wiki_root = vim.fn.stdpath 'state' .. '/wiki'
+  local wiki_dir = vim.fn.stdpath 'state' .. '/wiki'
+  if vim.fn.empty(vim.fn.glob(wiki_dir)) > 0 then
+    vim.fn.system { 'mkdir', '-p', wiki_dir }
+  end
+
+  vim.g.wiki_root = wiki_dir
 end
 
 return M
