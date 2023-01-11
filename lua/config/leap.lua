1256fbfa8cf4b4628560fca27ce0873b272f62dd
Alejandro Anzola <alejandro.anzola@mercadolibre.com.co>
2023-01-11 15:55:25 -0500

    configs leap plugin to not use "x" or "X" as part of its mappings

--- lua/config/leap.lua
+++ lua/config/leap.lua
@@ -2,6 +2,12 @@ local M = {}
 
 function M.setup()
   require('leap').add_default_mappings()
+
+  pcall(vim.keymap.del, { 'x', 'o' }, 'x')
+  pcall(vim.keymap.del, { 'x', 'o' }, 'X')
+
+  vim.keymap.set({ 'x', 'o' }, '<leader>s', '<Plug>(leap-forward-till)')
+  vim.keymap.set({ 'x', 'o' }, '<leader>S', '<Plug>(leap-backward-till)')
 end
 
 return M
