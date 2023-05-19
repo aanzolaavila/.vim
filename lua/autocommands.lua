eb56df230fd46445f0e8cf602eb4c650c4914cf4
Alejandro Anzola <me@alejandroanzola.info>
2023-05-19 00:08:04 -0500

    removes packer related autocmd

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -35,11 +35,3 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = highlight_group,
   pattern = '*',
 })
-
--- Automatically source and re-compile packer whenever you save this init.lua
-local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
-vim.api.nvim_create_autocmd('BufWritePost', {
-  command = 'source <afile> | PackerCompile',
-  group = packer_group,
-  pattern = vim.fn.expand '$MYVIMRC',
-})
