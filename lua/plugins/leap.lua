6b57df40cb196183adaeb2fce94bc6d0344db17e
Alejandro Anzola <me@alejandroanzola.info>
2023-05-22 02:06:57 -0500

    lazy loads leap.nvim plugin

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,12 +1,17 @@
 return {
   'ggandor/leap.nvim',
+  lazy = true,
+  keys = {
+    { 's',         '<Plug>(leap-forward-to)',    mode = { 'n', 'x', 'o' } },
+    { 'S',         '<Plug>(leap-backward-to)',   mode = { 'n', 'x', 'o' } },
+    { 'gs',        '<Plug>(leap-from-window)',   mode = { 'n', 'x', 'o' } },
+    { '<leader>s', '<Plug>(leap-forward-till)',  mode = { 'x', 'o' } },
+    { '<leader>S', '<Plug>(leap-backward-till)', mode = { 'x', 'o' } },
+  },
   config = function()
-    require('leap').add_default_mappings()
+    -- require('leap').add_default_mappings()
 
     pcall(vim.keymap.del, { 'x', 'o' }, 'x')
     pcall(vim.keymap.del, { 'x', 'o' }, 'X')
-
-    vim.keymap.set({ 'x', 'o' }, '<leader>s', '<Plug>(leap-forward-till)')
-    vim.keymap.set({ 'x', 'o' }, '<leader>S', '<Plug>(leap-backward-till)')
   end
 }
