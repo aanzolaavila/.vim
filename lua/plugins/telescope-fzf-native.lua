bffcc283b3ec4be9a0b086fc32d28ceb9169de5d
Alejandro Anzola <me@alejandroanzola.info>
2023-05-27 15:27:44 -0500

    refactors telescope fzf plugin config

--- lua/plugins/telescope-fzf-native.lua
+++ lua/plugins/telescope-fzf-native.lua
@@ -1,7 +1,13 @@
 -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
 return {
   'nvim-telescope/telescope-fzf-native.nvim',
-  lazy = true,
+  dependencies = {
+    'nvim-telescope/telescope.nvim',
+  },
+  event = "VeryLazy",
   build = 'make',
-  cond = vim.fn.executable 'make' == 1
+  cond = vim.fn.executable 'make' == 1,
+  config = function()
+    require('telescope').load_extension('fzf')
+  end
 }
