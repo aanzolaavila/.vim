695d4b9a18efefd454b4f88bff6a7d8787db59ce
Alejandro Anzola <me@alejandroanzola.info>
2024-02-04 19:54:25 -0500

    fixes rust-analyzer config

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -97,23 +97,30 @@ local servers = {
   },
 
   rust_analyzer = {
-    cargo = {
-      allFeatures = true,
-      loadOutDirsFromCheck = true,
-      runBuildScripts = true,
-    },
-    -- Add clippy lints for Rust.
-    checkOnSave = {
-      allFeatures = true,
-      command = "clippy",
-      extraArgs = { "--no-deps" },
-    },
-    procMacro = {
-      enable = true,
-      ignored = {
-        ["async-trait"] = { "async_trait" },
-        ["napi-derive"] = { "napi" },
-        ["async-recursion"] = { "async_recursion" },
+    ["rust-analyzer"] = {
+      cargo = {
+        allFeatures = true,
+        loadOutDirsFromCheck = true,
+        runBuildScripts = true,
+      },
+      -- Add clippy lints for Rust.
+      checkOnSave = {
+        allFeatures = true,
+        command = "clippy",
+        extraArgs = { "--no-deps" },
+      },
+      procMacro = {
+        enable = true,
+        ignored = {
+          ["async-trait"] = { "async_trait" },
+          ["napi-derive"] = { "napi" },
+          ["async-recursion"] = { "async_recursion" },
+        },
+      },
+      diagnostics = {
+        enable = true,
+        enableExperimental = true,
+        disabled = { "unresolved-proc-macro" },
       },
     },
   },
