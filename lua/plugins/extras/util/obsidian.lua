d364c0e08e2acc61e46c6f74bc0c27fc3a151275
Alejandro Anzola <me@alejandroanzola.info>
2024-02-27 00:47:47 -0500

    changes Obsidian config for templates

--- lua/plugins/extras/util/obsidian.lua
+++ lua/plugins/extras/util/obsidian.lua
@@ -48,7 +48,22 @@ return {
           end,
           opts = { buffer = true },
         },
-      }
+      },
+
+      templates = {
+        subdir = "templates",
+        date_format = "%Y-%m-%d",
+        time_format = "%H:%M",
+        -- A map for custom variables, the key should be the variable and the value a function
+        substitutions = {},
+      },
+
+      completion = {
+        -- Set to false to disable completion.
+        nvim_cmp = true,
+        -- Trigger completion at 2 chars.
+        min_chars = 2,
+      },
     },
   },
 }
