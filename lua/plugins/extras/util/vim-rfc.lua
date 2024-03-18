a3f44ca9c7fa1db911c1f801b5a8c49c01294688
Alejandro Anzola <aanzolaavila@creativa77.com.ar>
2024-03-18 15:31:39 +0000

    Configure vim-rfc plugin to be lazy loaded on command invocation

--- lua/plugins/extras/util/vim-rfc.lua
+++ lua/plugins/extras/util/vim-rfc.lua
@@ -2,5 +2,5 @@ return {
   -- 'mhinz/vim-rfc',
   'gierens/vim-rfc', -- This one fixes an issue with http 403 when doing the fetching
   branch = 'urllib-user-agent',
-  event = "VeryLazy",
+  cmd = { 'RFC' }
 }
