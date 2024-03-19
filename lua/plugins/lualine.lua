2557f8d644537c7555baf9dc7e384d362f22a26f
Alejandro Anzola <aanzolaavila@creativa77.com.ar>
2024-03-19 00:21:44 +0000

    Adds lsp-progress plugin to lualine
    To display current status of LSP server

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,29 +2,51 @@ return {
   'nvim-lualine/lualine.nvim', -- Fancier statusline
   priority = 1000,
   dependencies = {
-    'nvim-tree/nvim-web-devicons'
+    'nvim-tree/nvim-web-devicons',
+    {
+      'linrongbin16/lsp-progress.nvim',
+      config = function()
+        require('lsp-progress').setup()
+      end
+    },
   },
-  opts = {
-    options = {
-      globalstatus = true,
-      fmt = string.lower,
-      icons_enabled = true,
-      -- theme = 'habamax', -- use default one instead
+
+  config = function()
+    require('lualine').setup({
+      options = {
+        globalstatus = true,
+        fmt = string.lower,
+        icons_enabled = true,
+        -- theme = 'habamax', -- use default one instead
+      },
       sections = {
         lualine_a = {
           'mode',
-          fmt = function(str)
-            return str:sub(1, 1)
-          end
         },
         lualine_b = {
           'branch',
+          'diff',
+          'diagnostics',
         },
+        lualine_c = {
+          'filename',
+          require('lsp-progress').progress,
+        },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
       },
-    },
-    extensions = {
-      'nvim-tree',
-      'trouble',
-    },
-  },
+      extensions = {
+        'nvim-tree',
+        'trouble',
+      },
+    })
+
+    vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
+    vim.api.nvim_create_autocmd("User", {
+      group = "lualine_augroup",
+      pattern = "LspProgressStatusUpdated",
+      callback = require("lualine").refresh,
+    })
+  end
 }
