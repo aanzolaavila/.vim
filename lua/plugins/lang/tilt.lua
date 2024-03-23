682a1e6b7b0d212b98285ecfe71e8f04985f4bbf
Alejandro Anzola <me@alejandroanzola.info>
2024-03-23 00:13:00 -0500

    Configures Treesitter and LSP for Tilt
    Specifically with Tiltfile filetype, maybe should extend with general skylark filetypes

---
+++ lua/plugins/lang/tilt.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, {
+        "starlark",
+      })
+
+      local pattern = "Tiltfile*"
+      local cmd = { 'tilt', 'lsp', 'start' }
+      -- Add files/folders here that indicate the root of a project
+      local root_markers = { ".git", "Tiltfile" }
+      -- Change to table with settings if required
+      local settings = vim.empty_dict()
+
+      vim.api.nvim_create_autocmd("BufRead", {
+        pattern = pattern,
+        callback = function(args)
+          local match = vim.fs.find(root_markers, { path = args.file, upward = true })[1]
+          local root_dir = match and vim.fn.fnamemodify(match, ":p:h") or nil
+
+          vim.cmd [[ setlocal filetype=starlark ]]
+
+          vim.lsp.start({
+            name = "tiltlsp",
+            cmd = cmd,
+            root_dir = root_dir,
+            settings = settings,
+          })
+        end,
+      })
+    end,
+  },
+}
