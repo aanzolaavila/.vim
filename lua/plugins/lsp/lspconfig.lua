89c8f78492ef0919274c76755e9d8ad2eeed3690
Alejandro Anzola <aanzolaavila@swift-nav.com>
2024-03-26 22:48:22 +0000

    Adds new user command LspCapabilities
    To display LSP clients capabilities

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -139,6 +139,38 @@ return {
           }
         end,
       }
+
+      -- User command to list Lsp server capabilities
+      -- Taken from https://www.reddit.com/r/neovim/comments/13r7yzw/list_server_capabilities/
+      vim.api.nvim_create_user_command("LspCapabilities", function()
+        local clients = vim.lsp.get_clients()
+
+        for _, client in pairs(clients) do
+          if client.name ~= "null-ls" then
+            local capAsList = {}
+            for key, value in pairs(client.server_capabilities) do
+              if value and key:find("Provider") then
+                local capability = key:gsub("Provider$", "")
+                table.insert(capAsList, "  - " .. capability)
+              end
+            end
+
+            table.sort(capAsList) -- sorts alphabetically
+            local msg = "# " .. client.name .. "\n" .. table.concat(capAsList, "\n")
+            print(msg)
+
+            -- vim.notify(msg, vim.log.levels.TRACE, {
+            --   on_open = function(win)
+            --     local buf = vim.api.nvim_win_get_buf(win)
+            --     vim.api.nvim_buf_set_option(buf, "filetype", "markdown")
+            --   end,
+            --   timeout = 14000,
+            -- })
+
+            -- print(msg .. "\nCapabilities = " .. vim.inspect(client.server_capabilities))
+          end
+        end
+      end, {})
     end,
   },
   {
