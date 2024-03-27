3010663b078a763cbe6b49d0124702f27a0138af
Alejandro Anzola <aanzolaavila@swift-nav.com>
2024-03-27 16:02:08 +0000

    Adds new plugin for buffer management

---
+++ lua/plugins/extras/util/close-buffers.lua
@@ -0,0 +1,14 @@
+--[[
+Command 	Description 	Native version
+:Bdelete other 🔥 	bdelete all buffers except the buffer in the current window
+:Bdelete hidden 🔥 	bdelete buffers not visible in a window
+:Bdelete all 	bdelete all buffers 	:bufdo bdelete
+:Bdelete this 	bdelete buffer in the current window 	:bdelete
+:Bdelete nameless 	bdelete buffers without a name: [No Name]
+:Bdelete select 	Lets you interactively select which buffers to bdelete
+:Bdelete menu 	Lets you interactively choose one of the other commands above
+]]
+return {
+  'Asheq/close-buffers.vim',
+  event = 'VeryLazy',
+}
