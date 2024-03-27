a34203be0dfebd2117615e52014a51c12a4a5c7f
Alejandro Anzola <aanzolaavila@swift-nav.com>
2024-03-27 01:56:05 +0000

    Adds Bookmark telescope plugin keymaps

--- lua/plugins/telescope/telescope.lua
+++ lua/plugins/telescope/telescope.lua
@@ -43,19 +43,34 @@ return {
       {
         'MattesGroeger/vim-bookmarks',
         config = function()
-          vim.cmd [[
-            highlight BookmarkSign ctermbg=NONE ctermfg=160
-            highlight BookmarkLine ctermbg=194 ctermfg=NONE
+          -- FIXME: for some reason it is not working
+          vim.g.bookmark_no_default_key_mappings = 1
+          -- workaround
+          for _, keymap in ipairs({ 'mn', 'mp', 'ma', 'mkk', 'mjj' }) do
+            vim.keymap.del('n', keymap)
+          end
+          -- end workaround
 
-            let g:bookmark_save_per_working_dir = 1
-            let g:bookmark_auto_save = 1
-          ]]
+          vim.g.bookmark_save_per_working_dir = 1
+          vim.g.bookmark_auto_save = 1
+
+          -- default keymaps
+          vim.keymap.set('n', 'mm', '<CMD>BookmarkToggle<CR>', { desc = 'Add/remove bookmark at current line' })
+          vim.keymap.set('n', 'mi', '<CMD>BookmarkAnnotate<CR>', { desc = 'Add/edit/remove annotation at current line' })
+          vim.keymap.set('n', 'mc', '<CMD>BookmarkClear<CR>', { desc = 'Clear bookmarks in current buffer only' })
+          vim.keymap.set('n', 'mx', '<CMD>BookmarkClearAll<CR>', { desc = 'Clear bookmarks in all buffers' })
         end
       }
     },
     event = 'VeryLazy',
     config = function()
-      require('telescope').load_extension('vim_bookmarks')
+      local telescope = require('telescope')
+      telescope.load_extension('vim_bookmarks')
+
+      vim.keymap.set('n', '<leader>sb', telescope.extensions.vim_bookmarks.all,
+        { desc = '[s]earch [b]ookmarks' })
+      vim.keymap.set('n', '<leader>sB', telescope.extensions.vim_bookmarks.current_file,
+        { desc = '[s]earch [B]ookmarks in current file' })
     end,
   }
 }
