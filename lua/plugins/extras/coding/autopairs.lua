d3dc026d1d2640908f9acb6e240e48ceb4aedb13
Alejandro Anzola <me@alejandroanzola.info>
2024-04-03 19:47:26 -0500

    Improves autopairs plugin config

--- lua/plugins/extras/coding/autopairs.lua
+++ lua/plugins/extras/coding/autopairs.lua
@@ -1,10 +1,31 @@
 return {
   -- Autopairs
   "windwp/nvim-autopairs",
-  event = "VeryLazy",
+  dependencies = {
+    'hrsh7th/nvim-cmp',
+  },
+  event = "InsertEnter",
   config = function()
-    require("nvim-autopairs").setup {
-      check_ts = true
-    }
+    local npairs = require('nvim-autopairs')
+    npairs.setup({
+      check_ts = true,
+      --[[ FAST WRAP
+           Before        Input                    After         Note
+           -----------------------------------------------------------------
+           (|foobar      <M-e> then press $       (|foobar)
+           (|)(foobar)   <M-e> then press q       (|(foobar))
+           (|foo bar     <M-e> then press qh      (|foo) bar
+           (|foo bar     <M-e> then press qH      (foo|) bar
+           (|foo bar     <M-e> then press qH      (foo)| bar    if cursor_pos_before = false ]]
+      fast_wrap = {},
+    })
+
+    -- If you want insert `(` after select function or method item
+    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+    local cmp = require('cmp')
+    cmp.event:on(
+      'confirm_done',
+      cmp_autopairs.on_confirm_done()
+    )
   end
 }
