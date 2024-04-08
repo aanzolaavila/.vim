5dffd02082f34234eabd4ed9b8e581cf601a7453
Alejandro Anzola <me@alejandroanzola.info>
2024-04-08 01:17:53 -0500

    Sets specific tag version for treesitter

--- lua/plugins/treesitter/treesitter.lua
+++ lua/plugins/treesitter/treesitter.lua
@@ -3,6 +3,7 @@ return {
     -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     event = 'VeryLazy',
+    tag = 'v0.9.2',
     build = function()
       pcall(require('nvim-treesitter.install').update { with_sync = true })
     end,
@@ -49,10 +50,10 @@ return {
         incremental_selection = {
           enable = true,
           keymaps = {
-            init_selection = '<c-space>',
-            node_incremental = '<c-space>',
-            scope_incremental = '<c-space>',
-            node_decremental = '<c-backspace>',
+            init_selection = '<C-space>',
+            node_incremental = '<C-space>',
+            scope_incremental = '<C-space>',
+            node_decremental = '<C-backspace>',
           },
         },
 
