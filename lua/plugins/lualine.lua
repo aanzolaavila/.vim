7f14977c2cd686296b04236258ff3b38df6aab09
Alejandro Anzola <me@alejandroanzola.info>
2024-04-09 19:26:35 -0500

    Configures lualine -> lsp-progress plugin to show active lsp clients

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,15 +3,73 @@ return {
   priority = 1000,
   dependencies = {
     'nvim-tree/nvim-web-devicons',
+    'AndreM222/copilot-lualine',
     {
       'linrongbin16/lsp-progress.nvim',
-      config = function()
-        require('lsp-progress').setup()
-      end
+      main = 'lsp-progress',
+      opts = {
+        -- show all active lsp clients on the statusline
+        -- taken from: https://github.com/linrongbin16/lsp-progress.nvim/wiki/Advanced-Configuration#always-show-lsp-client-names
+        -- begin
+        client_format = function(client_name, spinner, series_messages)
+          if #series_messages == 0 then
+            return nil
+          end
+          return {
+            name = client_name,
+            body = spinner .. " " .. table.concat(series_messages, ", "),
+          }
+        end,
+        format = function(client_messages)
+          --- @param name string
+          --- @param msg string?
+          --- @return string
+          local function stringify(name, msg)
+            return msg and string.format("%s %s", name, msg) or name
+          end
+
+          local sign = "" -- nf-fa-gear \uf013
+          local lsp_clients = vim.lsp.get_clients()
+          local messages_map = {}
+          for _, climsg in ipairs(client_messages) do
+            messages_map[climsg.name] = climsg.body
+          end
+
+          if #lsp_clients > 0 then
+            table.sort(lsp_clients, function(a, b)
+              ---@diagnostic disable-next-line: undefined-field
+              return a.name < b.name
+            end)
+            local builder = {}
+            for _, cli in ipairs(lsp_clients) do
+              if
+                  type(cli) == "table"
+                  ---@diagnostic disable-next-line: undefined-field
+                  and type(cli.name) == "string"
+                  ---@diagnostic disable-next-line: undefined-field
+                  and string.len(cli.name) > 0
+              then
+                ---@diagnostic disable-next-line: undefined-field
+                if messages_map[cli.name] then
+                  table.insert(
+                    builder,
+                    ---@diagnostic disable-next-line: undefined-field
+                    stringify(cli.name, messages_map[cli.name])
+                  )
+                else
+                  ---@diagnostic disable-next-line: undefined-field
+                  table.insert(builder, stringify(cli.name))
+                end
+              end
+            end
+            if #builder > 0 then
+              return sign .. " " .. table.concat(builder, ", ")
+            end
+          end
+          return ""
+        end, -- end
+      }
     },
-    {
-      'AndreM222/copilot-lualine',
-    }
   },
 
   config = function()
