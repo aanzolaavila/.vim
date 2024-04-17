f3a8235e9866fc63a91269fa9842c59d7f9bb7eb
Alejandro Anzola <me@alejandroanzola.info>
2024-04-16 23:43:37 -0500

    small change

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -1,8 +1,7 @@
--- LSP settings.
---  This function gets run when an LSP connects to a particular buffer.
-
 local globalAutoFmtEnabled = true;
 
+-- LSP settings.
+--  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(client, bufnr)
   -- NOTE: Remember that lua is a real programming language, and as such it is possible
   -- to define small helper and utility functions so you don't have to repeat yourself
