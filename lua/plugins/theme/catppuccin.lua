5691d747a9dcf332c6ff0efacedfaff4ab056883
Alejandro Anzola <me@alejandroanzola.info>
2024-07-20 19:33:41 -0500

    Sets colorscheme to load if at daylight
    Otherwise use default, which is dark

--- lua/plugins/theme/catppuccin.lua
+++ lua/plugins/theme/catppuccin.lua
@@ -3,6 +3,10 @@ return {
   name = "catppuccin",
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme "catppuccin"
+    vim.cmd([[colorscheme catppuccin-latte]])
+  end,
+  cond = function ()
+    local time = os.date("*t")
+    return time.hour > 6 and time.hour < 18
   end,
 }
