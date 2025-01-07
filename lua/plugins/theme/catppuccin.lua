6c026e00346e19f2bf7ba3a3f3be31e3ee5632cd
Alejandro Anzola <aanzolaavila@swift-nav.com>
2025-01-07 20:14:22 +0000

    Theme changes with a schedule

--- lua/plugins/theme/catppuccin.lua
+++ lua/plugins/theme/catppuccin.lua
@@ -4,10 +4,10 @@ return {
   priority = 1000,
   config = function()
     local time = os.date("*t")
-    local theme = "catppuccin"
+    local theme = "catppuccin-mocha"
 
     if time.hour > 6 and time.hour < 18 then
-      theme = "catppuccin-latte"
+      theme = "catppuccin-macchiato"
     end
 
     local cmd = "colorscheme " .. theme
