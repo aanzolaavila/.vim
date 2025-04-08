2aa3b880b208872c5bd49a8e40d4c41280855e87
Alejandro Anzola <me@alejandroanzola.info>
2025-04-07 19:06:33 -0500

    Configures todo-comments with some shortcuts

--- lua/plugins/extras/coding/todo-comments.lua
+++ lua/plugins/extras/coding/todo-comments.lua
@@ -24,4 +24,10 @@ return {
       pattern = [[\b(KEYWORDS)\b]],
     }
   },
+  keys = {
+    { "]t",         function() require("todo-comments").jump_next() end,                  desc = "Next Todo Comment" },
+    { "[t",         function() require("todo-comments").jump_prev() end,                  desc = "Previous Todo Comment" },
+    { "<leader>st", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME initial_mode=normal<cr>", desc = "[S]earch [t]odos (TODO,FIX,FIXME)" },
+    { "<leader>sT", "<cmd>TodoTelescope initial_mode=normal<cr>",                         desc = "[S]earch all [T]odos" },
+  }
 }
