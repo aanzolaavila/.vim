dacf3aa4ff4b2e3b59cf0b94cb661260fe4128f1
Alejandro Anzola <me@alejandroanzola.info>
2025-10-08 20:34:34 -0500

    Adds snippet for bash scripts

--- plugin/lang/bash.lua
+++ plugin/lang/bash.lua
@@ -4,3 +4,21 @@ local treesitter_required = vim.list_extend(vim.g.treesitter_ensure_installed or
 vim.g.treesitter_ensure_installed = treesitter_required
 
 vim.lsp.enable("bashls")
+
+-- snippets
+
+local ls = require 'luasnip'
+local s, i = ls.s, ls.insert_node
+local fmt = require 'luasnip.extras.fmt'.fmt
+
+ls.add_snippets("sh", {
+	s("script", fmt([[
+	#!/usr/bin/env bash
+	set -o errexit
+	set -o nounset
+	set -o pipefail
+
+	firstargument="${{1:-somedefaultvalue}}"
+
+	echo "$firstargument]], {})),
+})
